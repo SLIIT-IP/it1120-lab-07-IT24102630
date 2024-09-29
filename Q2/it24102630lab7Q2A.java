2359abf5ac82df44e634b40f15293f38f635e225
IT24102630 <malminimasha73@gmail.com>
2024-09-29 07:49:41 +0530

    Add files via upload

---
+++ Q2/it24102630lab7Q2A.java
@@ -0,0 +1,13 @@
+public class it24102630lab7Q2A{
+  public static void main(String[] args){
+
+     for(int i=0; i<=4; i++)
+     {
+        for(int J=0; j<5; j++)
+        {
+           System.out.println("$");
+        }
+     System.out.println();
+      }
+   }
+}
